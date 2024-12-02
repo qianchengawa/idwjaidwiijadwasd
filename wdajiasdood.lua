e6513e9fc6fa2b19fd55bac7075f478c97b70988
qianchengawa <147665730+qianchengawa@users.noreply.github.com>
2024-12-02 08:28:14 +0800

    Update wdajiasdood.lua

--- wdajiasdood.lua
+++ wdajiasdood.lua
@@ -1,6 +1,6 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-	Name = "TDM V9.05",
+	Name = "TDM V1.96",
 	Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
 	LoadingTitle = "TowerDefenseMacro",
 	LoadingSubtitle = "by 牢大",
